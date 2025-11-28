b7c8097191d106f1a9a7c1978dd0fc03a5925266
Jacob Nickerson <jacobnickerson817@gmail.com>
2025-11-28 12:06:16 -0500

    Update model to use the new lock queue

--- DQNModel/src/env_manager.py
+++ DQNModel/src/env_manager.py
@@ -9,7 +9,6 @@ message_dtype = np.dtype([
     ('board', np.uint64),
     ('moves', np.uint8),
     ('reward', np.double),
-    ('is_fresh', np.bool)
 ])
 
 class ParallelEnvManager:
@@ -31,7 +30,27 @@ class ParallelEnvManager:
         """
         Returns the entire message array
         """
-        arr = np.frombuffer(self.shm.getMessageBatch(), dtype=message_dtype)
+        return np.frombuffer(self.shm.getMessageBatch(), dtype=message_dtype)
+
+    def pop_results(self) -> np.ndarray:
+        res = self.shm.getMessage()
+        if not res:
+            return np.zeros(0,dtype=message_dtype)
+        arr = np.zeros(1,dtype=message_dtype)
+        arr[0]["id"] = res.id
+        arr[0]["board"] = res.board
+        arr[0]["moves"] = res.moves
+        arr[0]["reward"] = res.reward
+        return arr
+    
+    def get_initial_states(self) -> np.ndarray:
+        arr = np.zeros(self.num_envs, dtype=message_dtype)
+        read = 0
+        while read < self.num_envs:
+            res = self.pop_results()
+            read += len(res)
+            for r in res:
+                arr[r["id"]] = r
         return arr
 
     def reset_all(self):
--- DQNModel/src/train.py
+++ DQNModel/src/train.py
@@ -4,37 +4,36 @@ from env_manager import ParallelEnvManager
 import tensorflow as tf
 from time import time
 
-def train_dqn(agent: DQNAgent, env_manager: ParallelEnvManager, episodes: int, epsilon: float):
+def train_dqn(agent: DQNAgent, env_manager: ParallelEnvManager, epsilon: float):
     num_envs = env_manager.num_envs
 
-    for ep in range(episodes):
-        print(ep)
+    episode = 0
+    while True:
+        episode += 1
+        print(f"episode: {episode}")
         active_envs = set(range(num_envs))
         env_manager.reset_all()  # reset all environments at episode start
         
         # Load initial states and set up some book-keeping structures
-        states = env_manager.poll_results()
+        states = env_manager.get_initial_states()
         needs_action = [True] * num_envs # used to prevent reprocessing states
         actions = [-1]*num_envs
 
         while active_envs:
             for i in active_envs:
-                state = states[i]
-                if needs_action[i] and state["is_fresh"]:
+                if needs_action[i]:
+                    state = states[i]
                     action = agent.select_action(state["board"],epsilon,state["moves"])
                     actions[states[i]["id"]] = action
                     needs_action[i] = False # mark that state as processed
 
             env_manager.write_actions(actions)
 
-            results = env_manager.poll_results()  # returns list of (env_idx, next_state, reward, done, is_fresh)
+            results = env_manager.pop_results()  # returns list of (env_idx, next_state, reward, done, is_fresh)
 
             for result in results:
-                env_idx, next_state, valid_moves, reward, is_fresh = result
-                if not is_fresh:
-                    continue
+                env_idx, next_state, valid_moves, reward = result
                 action = -actions[env_idx] # sent actions will be negated by write_actions
-                actions[env_idx] = -1
                 if action != 0b00010000:  # skip terminal states
                     agent.replay_buffer.add(states[env_idx]["board"], action, reward, next_state, valid_moves == 0b00010000)
                     states[env_idx] = result
@@ -45,7 +44,12 @@ def train_dqn(agent: DQNAgent, env_manager: ParallelEnvManager, episodes: int, e
             agent.update()
 
         agent.sync_target_network()
-    print("FINISHED!")
+
+        if episode % 100 == 0:
+            q_net_filename = f"saved_models/dqn_policy_ep_{episode}.weights.h5"
+            target_net_filename = f"saved_models/dqn_target_ep_{episode}.weights.h5"
+            agent.q_network.save_weights(q_net_filename)
+            agent.target_network.save_weights(target_net_filename)
 
 
 if __name__ == "__main__":
@@ -56,7 +60,7 @@ if __name__ == "__main__":
     else:
         print("GPU not detected")
 
-    NUM_ENVS = 15
+    NUM_ENVS = 1
     NUM_EP = 20
     STATE_DIM = 16
     ACTION_DIM = 4
@@ -69,9 +73,7 @@ if __name__ == "__main__":
     agent_2048 = DQNAgent(STATE_DIM, ACTION_DIM)
     env_man = ParallelEnvManager(NUM_ENVS)
 
-    tf.profiler.experimental.start("logs/profile")
-    train_dqn(agent_2048, env_man, episodes=NUM_EP, epsilon=0.1)
-    tf.profiler.experimental.stop()
-    agent_2048.q_network.save_weights("saved_models/dqn_policy.weights.h5")
-    agent_2048.target_network.save_weights("saved_models/dqn_target.weights.h5")
+    # tf.profiler.experimental.start("logs/profile")
+    train_dqn(agent_2048, env_man, epsilon=0.1)
+    # tf.profiler.experimental.stop()
     # training_sim.terminate()
