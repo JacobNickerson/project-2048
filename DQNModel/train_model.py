29c23d6dc2cb09faf64efd93af536db168eda3fc
Jacob Nickerson <jacobnickerson817@gmail.com>
2025-12-01 01:28:45 -0500

    Change saving to occur based on steps instead of episodes

--- DQNModel/train_model.py
+++ DQNModel/train_model.py
@@ -15,10 +15,10 @@ def main():
         print("GPU not detected")
 
     parser = ArgumentParser()
-    parser.add_argument("--num-env",type=int,required=True)
+    parser.add_argument("--num-env",type=int,required=False,default=1)
     parser.add_argument("--epsilon",type=float,required=False,default=0.1)
     parser.add_argument("--env-type",type=str,required=False,default="py")
-    parser.add_argument("--ep-save-interval",type=int,required=False,default=100)
+    parser.add_argument("--step-save-interval",type=int,required=False,default=10000)
     parser.add_argument("--ep-count",type=int,required=False,default=float('inf'))
     parser.add_argument("--output",type=str,required=False,default="model")
     args = parser.parse_args()
@@ -36,15 +36,16 @@ def main():
     match(args.env_type):
         case "py":
             env_man = PyEnvManager(args.num_env)
-            train_python_dqn(agent_2048, env_man, epsilon=args.epsilon, save_every=args.ep_save_interval,episode_count=args.ep_count, file_name=args.output)
+            train_python_dqn(agent_2048, env_man, epsilon=args.epsilon, save_every=args.step_save_interval,episode_count=args.ep_count, file_name=args.output)
         case "cpp":
+            # currently broken... shared memory structures don't populate properly when launching via popen
             # training_sim = subprocess.Popen(
-            #     ["../../DQNTrainer/build/DQNTrainer", f"{NUM_ENVS}"],
+            #     ["../build/DQNTrainer", f"{args.num_env}"],
             #     stdout = subprocess.PIPE,
             #     stderr = subprocess.PIPE
             # )
             env_man = CPPEnvManager(args.num_env)
-            train_dqn(agent_2048, env_man, epsilon=args.epsilon, save_every=args.ep_save_interval,episode_count=args.ep_count, file_name=args.output)
+            train_dqn(agent_2048, env_man, epsilon=args.epsilon, save_every=args.step_save_interval,episode_count=args.ep_count, file_name=args.output)
         case _:
             print(f"Environment type {args.env_type} not recognized, valid options: \"py\" and \"cpp\"")
 
