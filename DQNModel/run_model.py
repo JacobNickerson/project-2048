5248c6116388afbc01239502a19901909ea5e153
Jacob Nickerson <jacobnickerson817@gmail.com>
2025-12-01 14:34:41 -0500

    Add user input

--- DQNModel/run_model.py
+++ DQNModel/run_model.py
@@ -5,7 +5,8 @@ import numpy as np
 from argparse import ArgumentParser
 from src.agent import DQNAgent, RandomAgent
 from src.env_manager import CPPEnvManager, PyEnvManager
-from src.play import play_dqn, play_py_dqn
+from src.play import play_dqn, play_py_dqn, play_user_dqn
+
 
 def main():
     gpus = tf.config.list_physical_devices("GPU")
@@ -19,40 +20,77 @@ def main():
     ACTION_DIM = 4
 
     parser = ArgumentParser()
-    parser.add_argument("--random", type=bool, required=False, default=False)
-    parser.add_argument("--env-type",type=str,required=False,default="py")
-    parser.add_argument("--network", type=str, required=False, help="Path to the common base of the network weights files")
-    parser.add_argument("--q-network", type=str, required=False, help="Path to the q-network file")
-    parser.add_argument("--target-network", type=str, required=False, help="Path to the target network file")
-    parser.add_argument("--average-runs", type=int, required=False, help="Optionally run many runs and collect averages")
+    parser.add_argument(
+        "--input",
+        type=str,
+        required=True,
+        default=False,
+        help="Method of providing input, valid options: network, random, user",
+    )
+    parser.add_argument("--env-type", type=str, required=False, default="py")
+    parser.add_argument(
+        "--network",
+        type=str,
+        required=False,
+        help="Path to the common base of the network weights files",
+    )
+    parser.add_argument(
+        "--q-network", type=str, required=False, help="Path to the q-network file"
+    )
+    parser.add_argument(
+        "--target-network",
+        type=str,
+        required=False,
+        help="Path to the target network file",
+    )
+    parser.add_argument(
+        "--average-runs",
+        type=int,
+        required=False,
+        help="Optionally run many runs and collect averages",
+    )
     args = parser.parse_args()
-    if not args.random:
-        if not args.network and not (args.q_network and args.target_network):
-            print("error: --network or --q-network and --target-network required if not running with random input")
-            return
-        if args.network and (args.q_network or args.target_network):
-            print("error: --network not compatible with --q-network or --target-network")
-            return
-        agent = DQNAgent(STATE_DIM, ACTION_DIM)
-        if args.network:
-            q_network = args.network + "_policy.weights.h5"
-            target_network = args.network + "_target.weights.h5"
-        else:
-            q_network = args.q_network
-            target_network = args.target_network
-        print(f"Loading Q-Network: {q_network}")
-        print(f"Loading Target-Network: {target_network}")
-        agent.load_weights(q_network, target_network)
-    else:
-        agent = RandomAgent(STATE_DIM, ACTION_DIM)
-        print("Playing with random inputs")
+    input_method = args.input
 
-    match(args.env_type):
+    match (input_method):
+        case "random":
+            print("Playing with random inputs")
+            agent = RandomAgent(STATE_DIM, ACTION_DIM)
+        case "user":
+            print("Playing with user input")
+            play_user_dqn()
+            return
+        case "network":
+            if not args.network and not (args.q_network and args.target_network):
+                print(
+                    "error: --network or --q-network and --target-network required if not running with random input"
+                )
+                return
+            if args.network and (args.q_network or args.target_network):
+                print(
+                    "error: --network not compatible with --q-network or --target-network"
+                )
+                return
+            agent = DQNAgent(STATE_DIM, ACTION_DIM)
+            if args.network:
+                q_network = args.network + "_policy.weights.h5"
+                target_network = args.network + "_target.weights.h5"
+            else:
+                q_network = args.q_network
+                target_network = args.target_network
+            print(f"Loading Q-Network: {q_network}")
+            print(f"Loading Target-Network: {target_network}")
+            agent.load_weights(q_network, target_network)
+        case _:
+            print("error: invalid input method, valid options: network, random, user")
+            return
+
+    match (args.env_type):
         case "py":
             env_man = PyEnvManager(1)
             env = env_man.envs[0]
             if not args.average_runs:
-                play_py_dqn(agent,env)
+                play_py_dqn(agent, env)
                 print("RESULTS")
                 print("END STATE: ")
                 env.print_board()
@@ -65,7 +103,7 @@ def main():
                 for i in range(args.average_runs):
                     print(f"Game {i} in progress...")
                     env.reset()
-                    play_py_dqn(agent,env)
+                    play_py_dqn(agent, env)
                     unpacked_board = env.get_board(packed=False)
                     scores.append(env.score)
                     max_tiles.append(1 << unpacked_board[np.argmax(unpacked_board)])
@@ -86,12 +124,12 @@ def main():
         case "cpp":
             # TODO: Implement averaging, API is different between the two ENVs so its not trivial to just replace the env
             env_man = CPPEnvManager(1)
-            play_dqn(agent,env_man)
-        case "user_input":
-            play_user_dqn()
+            play_dqn(agent, env_man)
         case _:
-            print(f"Environment type {args.env_type} not recognized, valid options: \"py\" and \"cpp\"")
-    
+            print(
+                f'Environment type {args.env_type} not recognized, valid options: "py" and "cpp"'
+            )
+
 
 if __name__ == "__main__":
     main()
