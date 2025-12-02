941ee13b8a466d9ff0f8b249faf235ec25119deb
Jacob Nickerson <jacobnickerson817@gmail.com>
2025-12-01 23:26:31 -0500

    Update graph generation script

--- DQNModel/data_visualizer.py
+++ DQNModel/data_visualizer.py
@@ -35,14 +35,14 @@ def generate_graphs(data_set_name, scores, max_tiles, board_states):
     )
     plt.close()
 
-    tile_ticks = range(4, 13)
-
+    tiles = np.array([16, 32, 64, 128, 256, 512, 1024, 2048, 4096])
+    log_tiles = np.log2(tiles)
     # Max tile histogram
     plt.figure()
     plt.hist(np.log2(max_tiles), bins=30)
-    plt.xlabel("Max Tile Value (log2)")
+    plt.xlabel("Max Tile Value")
     plt.ylabel("Count")
-    plt.xticks(tile_ticks)
+    plt.xticks(log_tiles, tiles)
     plt.title(f"{data_set_name} Max Tile Distribution")
     plt.savefig(
         f"graphs/{data_set_name.replace(' ','_').lower()}_max_tiles.png",
@@ -51,12 +51,10 @@ def generate_graphs(data_set_name, scores, max_tiles, board_states):
     )
     plt.close()
 
-    tiles = np.array([128, 256, 512, 1024, 2048])
-    log_tiles = np.log2(tiles)
     probs = [(np.log2(max_tiles) >= t).mean() for t in log_tiles]
     plt.figure()
     plt.bar(log_tiles, probs)
-    plt.xlabel("Tile Value (log2)")
+    plt.xlabel("Tile Value")
     plt.ylabel("Probability")
     plt.xticks(log_tiles, tiles)
     plt.title(f"{data_set_name} Tile-Reach Probability")
@@ -86,7 +84,7 @@ def main():
     highest_tiles = []
     for step_count in step_counts:
         file_base = args.file_name
-        file_name = f"{step_count}_{file_base}"
+        file_name = f"raw_data/{step_count}_{file_base}"
         scores, max_tiles, board_states = read_file(file_name)
         generate_graphs(
             f"{step_count} Million Step Trained", scores, max_tiles, board_states
@@ -99,8 +97,7 @@ def main():
         highest_tiles.append(np.max(max_tiles))
 
     plt.figure()
-    plt.bar(range(len(step_counts)), average_scores)
-    plt.xticks(range(len(step_counts)), step_counts)
+    plt.plot(step_counts, average_scores)
     plt.xlabel("Training Steps (million steps)")
     plt.ylabel("Average Score")
     plt.title("Average Score vs. Training Steps")
@@ -109,8 +106,7 @@ def main():
     plt.close()
 
     plt.figure()
-    plt.bar(range(len(step_counts)), median_scores)
-    plt.xticks(range(len(step_counts)), step_counts)
+    plt.plot(step_counts, median_scores)
     plt.xlabel("Training Steps (million steps)")
     plt.ylabel("Median Score")
     plt.title("Median Score vs. Training Steps")
@@ -119,8 +115,7 @@ def main():
     plt.close()
 
     plt.figure()
-    plt.bar(range(len(step_counts)), high_scores)
-    plt.xticks(range(len(step_counts)), step_counts)
+    plt.plot(step_counts, high_scores)
     plt.xlabel("Training Steps (million steps)")
     plt.ylabel("Highest Score")
     plt.title("Highest Score vs. Training Steps")
@@ -128,37 +123,24 @@ def main():
     plt.savefig("graphs/high_score_vs_steps_bar.png", dpi=300, bbox_inches="tight")
     plt.close()
 
-    tile_ticks = np.array([128, 256, 512, 1024, 2048, 4096])
+    tile_ticks = np.array([32, 64, 128, 256, 512, 1024, 2048, 4096])
     plt.figure()
-    plt.bar(range(len(step_counts)), median_max_tiles)
+    plt.plot(step_counts, median_max_tiles)
     plt.yscale("log", base=2)
-    plt.xticks(range(len(step_counts)), step_counts)
     plt.yticks(tile_ticks, tile_ticks)
     plt.xlabel("Training Steps (million steps)")
-    plt.ylabel("Median Max Tiles")
+    plt.ylabel("Median Max Tile")
     plt.title("Median Max Tile vs. Training Steps")
     plt.tight_layout()
     plt.savefig("graphs/med_tile_vs_steps_bar.png", dpi=300, bbox_inches="tight")
     plt.close()
 
-    # plt.figure()
-    # plt.bar(range(len(step_counts)), highest_tiles)
-    # plt.xticks(range(len(step_counts)), step_counts)
-    # plt.yticks(tile_ticks)
-    # plt.xlabel("Training Steps (million steps)")
-    # plt.ylabel("Highest Max Tile")
-    # plt.title("Highest Max Tile vs. Training Steps (Bar Chart)")
-    # plt.tight_layout()
-    # plt.savefig("graphs/high_tile_vs_steps_bar.png", dpi=300, bbox_inches="tight")
-    # plt.close()
-
     plt.figure()
-    plt.bar(range(len(step_counts)), highest_tiles)
+    plt.plot(step_counts, highest_tiles)
     plt.yscale("log", base=2)
-    plt.xticks(range(len(step_counts)), step_counts)
     plt.yticks(tile_ticks, tile_ticks)
     plt.xlabel("Training Steps (million steps)")
-    plt.ylabel("Highest Max Tiles")
+    plt.ylabel("Highest Max Tile")
     plt.title("Highest Max Tile vs. Training Steps")
     plt.tight_layout()
     plt.savefig("graphs/high_tile_vs_steps_bar.png", dpi=300, bbox_inches="tight")
